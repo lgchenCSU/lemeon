8f8d1623d9e5ef470b69fe90c2767dc70bd0bc6f
ChenLong <2512119987@qq.com>
2020-07-23 23:30:04 +0800

    master first commit

--- app/src/main/java/com/example/lemeon/MainActivity.java
+++ app/src/main/java/com/example/lemeon/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //master branch here1
+        //master branch here2
+        //master branch here3
+        //master branch here4
+        //master branch here5
     }
 }
